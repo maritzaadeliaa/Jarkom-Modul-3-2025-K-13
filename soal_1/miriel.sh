70f23df27d367a9699bd51821776d08b343d18e5
mamettdd <mamettdd@gmail.com>
2025-11-02 22:47:54 +0700

    Delete soal_1 directory

--- soal_1/miriel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ip link set eth0 up
-ip addr flush dev eth0
-ip addr add 10.70.1.23/24 dev eth0
-ip route replace default via 10.70.1.1
-echo "nameserver 192.168.122.1" > /etc/resolv.conf
-ping -c 2 8.8.8.8; ping -c 2 google.com
